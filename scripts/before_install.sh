249a29d0b881cf16e7f90ceb3257b59bad079886
Alhassan Ibrahim <zerosoftwere@gmail.com>
2024-06-09 18:47:58 +0100

    Fix: Fixed scripts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 image_id=$(docker images -q flask_app)
-if [ -x "${image_id}" ]; then
+if [ -n "${image_id}" ]; then
     echo Removing image ${image_id}
     docker rmi ${image_id}
 else
--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 container_id=$(docker ps -q --filter 'name=flask_app')
-if [ -x "${container_id}" ]; then
+if [ -n "${container_id}" ]; then
     echo Removing container ${image_id}
     docker stop ${container_id}
     docker rm ${container_id}
