a40fadc7d0ae83ff1a1af6cb43165c9c67368adc
Alhassan Ibrahim <zerosoftwere@gmail.com>
2024-06-09 22:10:31 +0100

    Fix: Fixed container_id fetch

--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-container_id=$(docker ps -q --filter 'name=flask_app')
+container_id=$(docker ps -qa --filter 'name=flask_app')
 if [ -n "${container_id}" ]; then
     echo Removing container ${image_id}
     docker stop ${container_id}
