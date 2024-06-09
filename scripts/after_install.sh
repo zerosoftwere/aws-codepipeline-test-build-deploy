377a28a98cb6067b9a34835ef7359963a96cad70
Alhassan Ibrahim <zerosoftwere@gmail.com>
2024-06-09 20:44:04 +0100

    Fix: Fixed scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
+cd /home/ec2-user
 echo "Working directory $(pwd)"
 
-env
-
-docker ps -a
-whoami
-
-cat /home/ec2-user/Dockerfile
-
-docker build -t flask_app -f Dockerfile /home/ec2-user/
+docker build -t flask_app .
